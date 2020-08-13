9f42848b23119c6fe423fff016200478ec4934bd
Gabby Vicas <gabbyvicas@Gabbys-MacBook-Pro.local>
2020-08-13 16:43:42 -0500

    Completed through Task 4

--- index.js
+++ index.js
@@ -7,11 +7,15 @@ const breakfastBurrito = {name: "Breakfast Burrito", price: 16, category:"Breakf
 /* Task 1a: write a function to return more menu items with the same format as the items above. */
 
 function createMenuItem(name, cost, category){
-    /* Code here */
+    return { name, cost, category};
 }
 
+console.log(createMenuItem)
 /* Task 1b: use your function to create 3 more menu items. You may add any items to the menu that you'd like */
 
+console.log(createMenuItem('tacos', 11, 'Lunch'));
+console.log(createMenuItem('fuit cup', 3, 'Snacks'));
+console.log(createMenuItem('Curry', 1, 'Dinner' ));
 
 
 /* Task 2: You're having a lunch special! 25% off for teachers and students, 10% off for everyone else. Add a method to your burger object that automatically calculates price given a string as a parameter. 
@@ -24,6 +28,16 @@ and should return a number.
 
 For example, burger.discount("teacher") would return 13.5 and burger.discount("public") would return 16.2*/
 
+function burgerDiscount(str){
+  if(str === 'teacher' || str === 'student'){
+    return ((burger.price) * .75);
+  }
+  else if (str === 'public'){
+    return ((burger.price) * .90);
+  }
+}
+
+console.log(burgerDiscount('student'));
 
 
 ///////////////Reviews (MVP)///////////////////
@@ -40,9 +54,18 @@ const reviews = [{name: "Daniela", rating: 5, feedback:"Beautiful atmosphere and
 
 /* Task 3: Console.log just Julius' feedback */
 
+console.log(reviews[5].feedback);
+
 
 /* Task 4: Add a new rating with your (fictitious) opinions of the restaurant in the same format as the reviews above. */
 
+function addReview (arr, name, rating, feedback){
+  arr.push({name, rating, feedback});
+  return arr;
+}
+
+
+console.log(addReview(reviews,'Gabby', 4, 'Wonderful, loved the atomosphere'));
 
 /* Task 5: Add the following feedback to Reyna's rating - "this place is chill with really cool people, great for getting work done on weekdays"
 
